fb599ae4e169f1ae3ffddc828adad98bd2a6fb0e
Daniel Copley <djcopley@users.noreply.github.com>
2021-05-05 12:30:58 -0700

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Daniel Copley on 5/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Daniel Copley on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
